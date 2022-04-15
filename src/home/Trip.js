92884d7f406553ac7aa23eb6c6776f2fb952488b
{shiva} <girishiva93@gmail.com>
2022-04-15 13:52:18 +0545

    Adding Trip Slider

--- src/home/Trip.js
+++ src/home/Trip.js
@@ -6,12 +6,12 @@ import "./css/home.css";
 
 const breakPoints = [
   { width: 1, itemsToShow: 1 },
-  { width: 550, itemsToShow: 2, itemsToScroll: 2 },
+  { width: 550, itemsToShow: 2 },
   { width: 768, itemsToShow: 3 },
   { width: 1200, itemsToShow: 4 },
 ];
 const Trip = () => {
-  const carouselRef = React.useRef(null);
+  const carouselRef = useRef(null);
   const onNextStart = (currentItem, nextItem) => {
     if (currentItem.index === nextItem.index) {
       // we hit the last item, go to first item
@@ -25,28 +25,32 @@ const Trip = () => {
     }
   };
   return (
-    <section>
-      <div className="container">
-        <h1>Most Selling Trip</h1>
-        <div className="App">
-          <Carousel
-            className="carousel-university"
-            breakPoints={breakPoints}
-            ref={carouselRef}
-            onPrevStart={onPrevStart}
-            onNextStart={onNextStart}
-            disableArrowsOnEnd={false}
-            autoPlaySpeed={1500}
-            enableAutoPlay
-            pagination={false}
-          >
-            {items.map((item) => (
-              <Item key={item.id}>{item.title}</Item>
-            ))}
-          </Carousel>
-        </div>
-      </div>
-    </section>
+    <div className="container">
+      <h1 className="trip-heading">Most Selling Trip</h1>
+      <Carousel
+        className="carousel-university"
+        breakPoints={breakPoints}
+        ref={carouselRef}
+        onPrevStart={onPrevStart}
+        onNextStart={onNextStart}
+        disableArrowsOnEnd={false}
+        autoPlaySpeed={1500}
+        enableAutoPlay
+        pagination={false}
+      >
+        {items.map((item, index) => (
+          <div key={item.id}>
+            <Item>
+              <img src={item.image} className="trip-img" alt={item.title} />
+            </Item>
+            <div className="trip-items-col">
+              <h3 className="trip-title">{item.title}</h3>
+              <p className="trip-date">{item.starton}</p>
+            </div>
+          </div>
+        ))}
+      </Carousel>
+    </div>
   );
 };
 
