1b0aa00afd113f50c7fe35e53178cda267305c1b
{shiva} <girishiva93@gmail.com>
2022-04-20 22:46:38 +0545

    Trekking Trips Category

--- src/trekking/Trekking.js
+++ src/trekking/Trekking.js
@@ -49,6 +49,76 @@ const Trekking = () => {
               <button className="explore-trek">Explore Easy Trek</button>
             </Col>
           </Row>
+          {/* Medium */}
+          <Row>
+            <Col md={5}>
+              {" "}
+              <h1 className="trekking-title-medium">
+                Moderate Treks
+                <span>We Make Your Trip Easy</span>
+              </h1>
+              <p className="easy-trek-desc">
+                {" "}
+                This category of treks requires basics outdoor experience and a
+                certain degree of physical aptitude. If you are physically fit
+                and are the kind of person who enjoys the outdoors, age is not
+                going to be an issue although it would be unwise for very young
+                children to attempt these treks. The trek in this category
+                entails an experienced camping crew who handles all logistical
+                issues. All you have to do is enjoy the adventure and wilderness
+                while the gritty details are worked out by our professional
+                crew.
+              </p>
+              <button className="explore-trek">Explore Moderate Trek</button>
+            </Col>
+            <Col md={7}>
+              <div className="easy-img-overlap-second">
+                <div className="medium-img-i">
+                  <img src={image1} alt="image1" width="100%" />
+                </div>
+                <div className="medium-img-ii">
+                  <img src={image2} alt="image1" width="100%" />
+                </div>
+                <div className="medium-img-iii">
+                  <img src={image3} alt="image1" width="100%" />
+                </div>
+              </div>
+            </Col>
+          </Row>
+          <Row>
+            <Col md={7}>
+              <div className="easy-img-overlap">
+                <div className="easy-img-i">
+                  <img src={image1} alt="image1" width="100%" />
+                </div>
+                <div className="easy-img-ii">
+                  <img src={image2} alt="image1" width="100%" />
+                </div>
+                <div className="easy-img-iii">
+                  <img src={image3} alt="image1" width="100%" />
+                </div>
+              </div>
+            </Col>
+            <Col md={5}>
+              {" "}
+              <h1 className="trekking-title">
+                Adventurous Treks
+                <span>We Make Your Trip Easy</span>
+              </h1>
+              <p className="easy-trek-desc">
+                {" "}
+                This category of treks is for those with a strong physique, a
+                lot of outdoor experience, and a taste for extreme adventures.
+                You have to be physically and psychologically prepared to
+                survive in an extreme environment under pressure. All the trips
+                are high pass treks of over 5000 meters and peak climbs over
+                6000 meters. We will do our utmost to ensure that you will
+                progress through challenging conditions without any physical or
+                mental stress to you.
+              </p>
+              <button className="explore-trek">Explore Adventurous Trek</button>
+            </Col>
+          </Row>
         </div>
       </section>
     </>
